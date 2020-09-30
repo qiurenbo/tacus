019325b5fc6873033d900988fd5ce68b74231a69
qiurenbo <lampqiu@sina.com>
2020-09-30 11:22:48 +0800

    feat(.d.ts)：support ts

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+export class Tacus {
+  constructor(options: { bufferSize: number; sampleRate: number });
+
+  start(): void;
+
+  stop(): void;
+
+  pause(): void;
+
+  resume(): void;
+
+  play(): void;
+
+  download(): void;
+}
